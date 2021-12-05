6fe63679e918f270f559b14ab81081966d3692d0
Омеляшко Егор <egor5352@gmail.com>
2021-12-05 22:05:26 +0400

    added fragment manager

--- app/src/main/java/com/example/storegames/MainActivity.java
+++ app/src/main/java/com/example/storegames/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.storegames;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.storegames.controller.GameFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new GameFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
